11a5f4a20e962f33848215e82a8a09e1b8ae0395
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-03-24 21:48:42 -0700

    docs(school-project): add documentation for Teacher class
    Don't know how I missed this in the last commit :v

--- school/src/main/java/Teacher.java
+++ school/src/main/java/Teacher.java
@@ -1,8 +1,18 @@
+/**
+ * Represents a teacher, containing information about their first and last name in addition to the subject they teach.
+ */
 public class Teacher {
     private String firstName;
     private String lastName;
     private String subject;
 
+    /**
+     * Creates a new teacher.
+     *
+     * @param firstName First name of the teacher.
+     * @param lastName  Last name of the subject.
+     * @param subject   The subject taught by this teacher.
+     */
     public Teacher(String firstName, String lastName, String subject) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -33,6 +43,9 @@ public class Teacher {
         this.subject = subject;
     }
 
+    /**
+     * Prints information about this teacher to standard output, using the format <code>Name: [full name] Subject: [subject]</code>.
+     */
     public void print() {
         System.out.println("Name: " + this.firstName + " " + this.lastName + " Subject: " + subject);
     }
