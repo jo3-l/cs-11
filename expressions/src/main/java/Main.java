8d4bd6013edfcf3728a1281b6c5ae9388dc23a4d
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-04-30 21:25:09 -0700

    style(expressions): clean up expressions assignment

--- expressions/src/main/java/Main.java
+++ expressions/src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         // Expression 2
         System.out.println(23 % 4 + (18 - 3 / 2));
-        // Same thing here - fractional part is discarded.
+        // Same thing here - the fractional part is discarded.
         System.out.println(23 % 4 + (18 - 1));
         System.out.println(23 % 4 + 17);
         System.out.println(3 + 17);
@@ -21,7 +21,8 @@ public class Main {
         // Expression 3
         System.out.println((8 - 5) + Math.pow(3, 4) / 5);
         System.out.println(3 + Math.pow(3, 4) / 5);
-        // Looking at `Math.pow()`'s method signature, we can see that it returns a double, so we append the 'd' prefix.
+        // Looking at `Math.pow()`'s method signature, we can see that it returns a double rather than an int.
+        // Thus, we append a 'd' to the end of the number literal.
         System.out.println(3 + 81d / 5);
         System.out.println(3 + 16.2d);
         System.out.println(19.2d);
@@ -41,8 +42,8 @@ public class Main {
         System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
         System.out.println(((7 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
         System.out.println((10 * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
-        // You may notice that we lose a few decimal digits here. That's because our calculation of sin(45 radians) is
-        // a bit less accurate than that of the computer. Hope that's fine!
+        // We lose a few decimal digits here, which is because our calculation of sin(45rad) is less accurate than that
+        // of the computer.
         System.out.println((10 * 0.85090352453d + 2) - Math.pow(Math.sqrt(25), 3));
         System.out.println((10 * 0.85090352453d + 2) - Math.pow(5d, 3));
         System.out.println((10 * 0.85090352453d + 2) - 125d);
