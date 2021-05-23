9a95dbadb53c41fb69f508593af6b832edce8b93
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-05-22 21:05:48 -0700

    docs(game-of-chance): add comments for Controller impl

--- game-of-chance/src/sample/Controller.java
+++ game-of-chance/src/sample/Controller.java
@@ -60,28 +60,44 @@ public class Controller implements Initializable {
         beginNewGame(false);
     }
 
+    // Generates a number that allows the user to have a `currentWinChance` probability of winning.
     private int genInt(GuessKind guess) {
+        // Generate a random probability between 0 and 1.
         double p = random.nextDouble();
 
+        // If it's within the win probability, then do our best to generate a number that causes the user to win.
         if (p <= currentWinChance) {
             if (guess.equals(GuessKind.HIGHER)) {
+                // If the current number is 6, there's no way the user can win, so just generate a number between 1 and 6.
                 if (currentNumber == 6) return randIntBetween(1, 6);
+
+                // Otherwise, generate a random integer that is greater than the current number.
                 int begin = currentNumber + 1;
                 return randIntBetween(begin, 6);
             }
 
+            // If the current number is 1 and the user picked "lower", then there's no way they can we. Again, just generate
+            // a random number between 1 and 6.
             if (currentNumber == 1) return randIntBetween(1, 6);
+
+            // Otherwise, generate a random integer that is less than the current number.
             return randIntBetween(1, currentNumber - 1);
         }
 
+        // Otherwise, do our best to make the user lose.
+
         if (guess.equals(GuessKind.HIGHER)) {
+            // If the user chose "Higher", than generate a number that is less than or equal to the current one.
             return randIntBetween(1, currentNumber);
         }
 
+        // Otherwise, generate a number that is greater than or equal to the current one.
         return randIntBetween(currentNumber, 6);
     }
 
     private void updateCurrentWinChance() {
+        // The user's chance of winning decreases as the round number increases. This function does its best to do that
+        // by using a tiered system: the chance of winning begins at 50% and decreases slowly over time.
         if (currentRound < 5) currentWinChance -= 0.03;
         else if (currentRound < 10) currentWinChance -= 0.02;
         else if (currentRound < 20) currentWinChance -= 0.015;
@@ -91,7 +107,9 @@ public class Controller implements Initializable {
     }
 
     private void beginNewGame(boolean didLose) {
+        // Only add the current winnings to the total if the user cashed out.
         if (!didLose) totalWinnings += currentWinnings;
+
         currentNumber = randIntBetween(1, 6);
         currentWinChance = 0.5;
         currentWinnings = 0;
@@ -109,10 +127,13 @@ public class Controller implements Initializable {
 
     private void setLabelText(Label label, String text) {
         label.setText(text);
+
+        // These lines ensure that 1) the label text fits and 2) it is still centered.
         label.setMinWidth(Region.USE_PREF_SIZE);
         label.setAlignment(Pos.CENTER);
     }
 
+    // randIntBetween generates a random integer in the range [begin, end]. It returns begin if begin == end.
     private int randIntBetween(int begin, int end) {
         if (begin == end) return begin;
         return begin + random.nextInt(end - begin + 1);
@@ -120,6 +141,7 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // Add "Higher" and "Lower" to the dropdown.
         higherLowerComboBox.getItems().addAll(GuessKind.values());
         beginNewGame(false);
     }
